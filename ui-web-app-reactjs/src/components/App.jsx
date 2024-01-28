7aa1ff6151a8ac8c579e651b494f3398b7a567df
LokeshJavvaji <lokeshjavvaji94@gmail.com>
2024-01-28 11:51:53 +0530

    modified app.jsx

--- ui-web-app-reactjs/src/components/App.jsx
+++ ui-web-app-reactjs/src/components/App.jsx
@@ -1,5 +1,12 @@
 import React, {useState, useEffect} from 'react'
 
+const Button = ({ label, onClick }) => {
+  return (
+    <button onClick={onClick}>
+      {label}
+    </button>
+  );
+};
 const App = () => {
 
   const [response, setResponse] = useState(null)
@@ -7,114 +14,100 @@ const App = () => {
   const [loading, setLoading] = useState(false)
   const [displayData, setDisplayData] = useState({url:''})
   const [details, setDetails] = useState('');
-
   
-  const getUserGeolocationDetails = () => {
-    fetch("/myip")
-        .then(response => response.json())
-        .then(data => {
-          console.log(data)
-          setDetails(data.url)
-        });
-  }
+    const shoesData = [
+      {
+        id: 1,
+        brand: 'Nike',
+        model: 'Air Max',
+        price: 99.99,
+        color: 'Black',
+      },
+      {
+        id: 2,
+        brand: 'Adidas',
+        model: 'Ultra Boost',
+        price: 129.99,
+        color: 'White',
+      },
+      {
+        id: 3,
+        brand: 'Puma',
+        model: 'RS-X',
+        price: 79.99,
+        color: 'Red',
+      },
+      {
+        id: 4,
+        brand: 'New Balance',
+        model: '990v5',
+        price: 149.99,
+        color: 'Grey',
+      },
+    ];
+  
+    const offersData = [
+      {
+        id: 1,
+        title: 'Back to School Sale',
+        discount: '20% off on all shoes',
+        expiryDate: '2024-09-30',
+      },
+      {
+        id: 2,
+        title: 'Summer Clearance',
+        discount: 'Buy one, get one 50% off',
+        expiryDate: '2024-08-15',
+      },
+    ];
+  
+    const cartData = [
+      {
+        id: 1,
+        brand: 'Reebok',
+        model: 'Classic Leather',
+        price: 69.99,
+        color: 'Blue',
+        quantity: 1,
+      },
+    ];
+  
+    const wishlistData = [
+      {
+        id: 1,
+        brand: 'Vans',
+        model: 'Old Skool',
+        price: 59.99,
+        color: 'Black/White',
+      },
+      {
+        id: 2,
+        brand: 'Converse',
+        model: 'Chuck Taylor All Star',
+        price: 49.99,
+        color: 'Red',
+      },
+    ];
+  
+    const handleButtonClick = (category) => {
+      switch (category) {
+        case 'shoes':
+          setDisplayedDetails(JSON.stringify(shoesData, null, 2));
+          break;
+        case 'offers':
+          setDisplayedDetails(JSON.stringify(offersData, null, 2));
+          break;
+        case 'cart':
+          setDisplayedDetails(JSON.stringify(cartData, null, 2));
+          break;
+        case 'wishlist':
+          setDisplayedDetails(JSON.stringify(wishlistData, null, 2));
+          break;
+        default:
+          setDisplayedDetails('');
+      }
+    };
 
-  useEffect(()=>{
-    getUserGeolocationDetails()
-  },[])
-  const [displayedDetails, setDisplayedDetails] = useState('');
-
-  const shoesData = [
-    {
-      id: 1,
-      brand: 'Nike',
-      model: 'Air Max',
-      price: 99.99,
-      color: 'Black',
-    },
-    {
-      id: 2,
-      brand: 'Adidas',
-      model: 'Ultra Boost',
-      price: 129.99,
-      color: 'White',
-    },
-    {
-      id: 3,
-      brand: 'Puma',
-      model: 'RS-X',
-      price: 79.99,
-      color: 'Red',
-    },
-    {
-      id: 4,
-      brand: 'New Balance',
-      model: '990v5',
-      price: 149.99,
-      color: 'Grey',
-    },
-  ];
-
-  const offersData = [
-    {
-      id: 1,
-      title: 'Back to School Sale',
-      discount: '20% off on all shoes',
-      expiryDate: '2024-09-30',
-    },
-    {
-      id: 2,
-      title: 'Summer Clearance',
-      discount: 'Buy one, get one 50% off',
-      expiryDate: '2024-08-15',
-    },
-  ];
-
-  const cartData = [
-    {
-      id: 1,
-      brand: 'Reebok',
-      model: 'Classic Leather',
-      price: 69.99,
-      color: 'Blue',
-      quantity: 1,
-    },
-  ];
-
-  const wishlistData = [
-    {
-      id: 1,
-      brand: 'Vans',
-      model: 'Old Skool',
-      price: 59.99,
-      color: 'Black/White',
-    },
-    {
-      id: 2,
-      brand: 'Converse',
-      model: 'Chuck Taylor All Star',
-      price: 49.99,
-      color: 'Red',
-    },
-  ];
-
-  const handleButtonClick = (category) => {
-    switch (category) {
-      case 'shoes':
-        setDisplayedDetails(JSON.stringify(shoesData, null, 2));
-        break;
-      case 'offers':
-        setDisplayedDetails(JSON.stringify(offersData, null, 2));
-        break;
-      case 'cart':
-        setDisplayedDetails(JSON.stringify(cartData, null, 2));
-        break;
-      case 'wishlist':
-        setDisplayedDetails(JSON.stringify(wishlistData, null, 2));
-        break;
-      default:
-        setDisplayedDetails('');
-    }
-  };
 
 
   return (
@@ -124,17 +117,17 @@ const App = () => {
         <div className="tc ttc">microservices-architect-config-starter</div>
         <div className="tc ttc">Sample Microservice calls</div>
       </header>
-      <div>
-      <Button label="Shoes Information" onClick={() => handleButtonClick('shoes')} />
-      <Button label="Offers Information" onClick={() => handleButtonClick('offers')} />
-      <Button label="Cart Information" onClick={() => handleButtonClick('cart')} />
-      <Button label="Wishlist Information" onClick={() => handleButtonClick('wishlist')} />
-
-      <div>
-        <h2>Displayed Details:</h2>
-        <pre>{displayedDetails}</pre>
+      <div className="pt4 pb1 tc">
+        Go save the world with JavaScript
+        <br/> <br/> <br/>
+        <button name="shoe/shoes" onClick={handleApiCall} style={{margin:'0px 10px'}}>Shoes</button>
+        <button name="offer/offers" onClick={handleApiCall} style={{margin:'0px 10px'}}>Offers</button>
+        <button name="cart" onClick={handleApiCall} style={{margin:'0px 10px'}}>Cart</button>
+        <button name="wishlist" onClick={handleApiCall} style={{margin:'0px 10px'}}>Wishlist</button>
+        <br/> <br/> <br/>
+        
+        
       </div>
-    </div>
     </>
   )
 }
