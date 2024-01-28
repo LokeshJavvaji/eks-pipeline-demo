1c2bdb26f97ef9d2e89a35bd613a3b02ae05e9a7
LokeshJavvaji <lokeshjavvaji94@gmail.com>
2024-01-28 13:20:10 +0530

    changes in app

--- ui-web-app-reactjs/src/components/App.jsx
+++ ui-web-app-reactjs/src/components/App.jsx
@@ -1,5 +1,12 @@
 import React, {useState, useEffect} from 'react'
 
+const Button = ({ label, onClick }) => {
+  return (
+    <button onClick={onClick}>
+      {label}
+    </button>
+  );
+};
 const App = () => {
 
   const [response, setResponse] = useState(null)
@@ -25,7 +32,80 @@ const App = () => {
   const handleApiCall = async (e) => {
     // const url = 'http://localhost:9999/'+e.target.name;
     let url = details+e.target.name
-
+    const shoesData = [
+      {
+        id: 1,
+        brand: 'Nike',
+        model: 'Air Max',
+        price: 99.99,
+        color: 'Black',
+      },
+      {
+        id: 2,
+        brand: 'Adidas',
+        model: 'Ultra Boost',
+        price: 129.99,
+        color: 'White',
+      },
+      {
+        id: 3,
+        brand: 'Puma',
+        model: 'RS-X',
+        price: 79.99,
+        color: 'Red',
+      },
+      {
+        id: 4,
+        brand: 'New Balance',
+        model: '990v5',
+        price: 149.99,
+        color: 'Grey',
+      },
+    ];
+  
+    const offersData = [
+      {
+        id: 1,
+        title: 'Back to School Sale',
+        discount: '20% off on all shoes',
+        expiryDate: '2024-09-30',
+      },
+      {
+        id: 2,
+        title: 'Summer Clearance',
+        discount: 'Buy one, get one 50% off',
+        expiryDate: '2024-08-15',
+      },
+    ];
+  
+    const cartData = [
+      {
+        id: 1,
+        brand: 'Reebok',
+        model: 'Classic Leather',
+        price: 69.99,
+        color: 'Blue',
+        quantity: 1,
+      },
+    ];
+  
+    const wishlistData = [
+      {
+        id: 1,
+        brand: 'Vans',
+        model: 'Old Skool',
+        price: 59.99,
+        color: 'Black/White',
+      },
+      {
+        id: 2,
+        brand: 'Converse',
+        model: 'Chuck Taylor All Star',
+        price: 49.99,
+        color: 'Red',
+      },
+    ];
+  
     displayData.url=url
     let options = {}
       try {
@@ -44,7 +124,24 @@ const App = () => {
       }
   }
 
-
+  const handleButtonClick = (category) => {
+    switch (category) {
+      case 'shoes':
+        setDisplayedDetails(JSON.stringify(shoesData, null, 2));
+        break;
+      case 'offers':
+        setDisplayedDetails(JSON.stringify(offersData, null, 2));
+        break;
+      case 'cart':
+        setDisplayedDetails(JSON.stringify(cartData, null, 2));
+        break;
+      case 'wishlist':
+        setDisplayedDetails(JSON.stringify(wishlistData, null, 2));
+        break;
+      default:
+        setDisplayedDetails('');
+    }
+  };
 
   return (
     <>
@@ -74,9 +171,20 @@ const App = () => {
           {error&&<>
             <p>API hit through API Gateway to  <span style={{color:'blue', fontSize:'18px'}}>{displayData.url}</span></p>
             <br/> <br/>
-            <p>--Error--</p>
+            <div>
+              <Button label="Shoes Information" onClick={() => handleButtonClick('shoes')} />
+              <Button label="Offers Information" onClick={() => handleButtonClick('offers')} />
+              <Button label="Cart Information" onClick={() => handleButtonClick('cart')} />
+              <Button label="Wishlist Information" onClick={() => handleButtonClick('wishlist')} />
+
+              <div>
+                <h2>Displayed Details:</h2>
+                <pre>{displayedDetails}</pre>
+              </div>
+            </div>
+
             {JSON.stringify(error)}
-            <p>Probably one of the microservice is down</p>
+            
           </>}
         </div>
       </div>
